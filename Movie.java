adf77090ee07c43d772bfff681c540e4a1ad3f61
vivihzg <vivihzgithub@gmail.com>
2024-11-25 15:02:06 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,20 +5,32 @@ public class Movie {
     public static final int  NEW_RELEASE = 1;
   
     private String _title;
-    private int _priceCode;
+    private Price _price;
   
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
   
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
   
     public void setPriceCode(int arg) {
-        _priceCode = arg;
-    }
+        switch (arg) {
+           case REGULAR:
+              _price = new RegularPrice();
+              break;
+           case CHILDRENS:
+              _price = new ChildrensPrice();
+              break;
+           case NEW_RELEASE:
+              _price = new NewReleasePrice();
+              break;
+           default:
+              throw new IllegalArgumentException("Incorrect Price Code");
+        }
+     }
   
     public String getTitle (){
         return _title;
@@ -28,7 +40,7 @@ public class Movie {
         double thisAmount = 0;
 
       // determine amounts for each line
-      switch (_priceCode) {
+      switch (_price.getPriceCode()) {
          case Movie.REGULAR:
             thisAmount += 2;
             if (daysRented > 2)
@@ -48,7 +60,7 @@ public class Movie {
 
     public int getFrequentRenterPoints(int daysRented){
          // add bonus for a two day new release rental
-      if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1) {
+      if ((_price.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
         return 2;
       }
       return 1;
