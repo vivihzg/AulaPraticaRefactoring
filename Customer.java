5271316d966484ef7e8aed56a02e6a6459904f24
vivihzg <vivihzgithub@gmail.com>
2024-11-25 14:19:27 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -17,21 +17,21 @@ public class Customer {
       return _name;
    }
 
-   public String statement() {
+   public String htmlStatement() {
       Enumeration<Rental> rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
+      String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
-
          // show figures for this rental
-         result += "\t" + each.getMovie().getTitle() + "\t" +
-                  String.valueOf(each.getCharge()) + "\n";
+         result += each.getMovie().getTitle()+ ": " +
+                  String.valueOf(each.getCharge()) + "<BR>\n";
       }
 
       // add footer lines
-      result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-            " frequent renter points";
+      result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+      result += "On this rental you earned <EM>" +
+            String.valueOf(getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
       return result;
    }
 
